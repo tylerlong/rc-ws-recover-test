b591cb3db65e98a1398002106e32be32a4d77981
Tyler Liu <tyler4long@gmail.com>
2023-11-03 14:51:08 -0700

    Finish the first experiment

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Untitled App
+# RingCentral WebSocket Recover Test
+
+
+## Experiment 1
+
+1. Connect to WS
+2. Save the wsc token. 
+3. Disconnect 
+4. Reconnect with the wsc token
+
+"recoveryState": "Failed",
+"recoveryErrorCode": "WSG-1005",
+
+原因是找不到这个 ws session 关联的 subscription id。
+
+
+## Experiment 2
+
+1. Connect to WS
+2. Save the wsc token. 
+3. Create a subscription
+4. Disconnect 
+5. Reconnect with the wsc token
+
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log('Hello world!');
+import RingCentral from '@rc-ex/core';
+import waitFor from 'wait-for-async';
+import WS from 'ws';
+// import hyperid from 'hyperid';
+// import waitFor from 'wait-for-async';
+
+// const uuid = hyperid();
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  await rc.authorize({ jwt: process.env.RINGCENTRAL_JWT_TOKEN! });
+  const r = await rc.post('/restapi/oauth/wstoken');
+  const wsToken = r.data as { uri: string; ws_access_token: string };
+
+  const wsUri = `${wsToken.uri}?access_token=${wsToken.ws_access_token}`;
+  const ws = new WS(wsUri);
+  let wscToken = '';
+  ws.addEventListener('message', (e) => {
+    console.log(JSON.stringify(JSON.parse(e.data as string), null, 2));
+    const message = e.data as string;
+    if (message.includes('"ConnectionDetails"')) {
+      wscToken = JSON.parse(message)[0].wsc.token;
+    }
+  });
+  await waitFor({ interval: 1000, condition: () => wscToken !== '' });
+  ws.close();
+  await waitFor({ interval: 5000 });
+  const r2 = await rc.post('/restapi/oauth/wstoken');
+  const wsToken2 = r2.data as { uri: string; ws_access_token: string };
+  const wsUri2 = `${wsToken2.uri}?access_token=${wsToken2.ws_access_token}&wsc=${wscToken}`;
+  const ws2 = new WS(wsUri2);
+  ws2.addEventListener('message', (e) => {
+    console.log(JSON.stringify(JSON.parse(e.data as string), null, 2));
+  });
+};
+
+main();
